547c6b20e7c774db5e186fb70ee6a064ebcf50a8
Fademacios <122602505+Fademacios@users.noreply.github.com>
2025-05-09 08:47:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
   const kolorInput = document.getElementById('kolor');
-  const form = document.getElementById('formularz');
 
-  kolorInput.addEventListener('input', function () {
-    document.body.style.backgroundColor = this.value;
-  });
-
-  form.addEventListener('submit', function (e) {
-    e.preventDefault(); // zapobiega przeładowaniu
-
-    const dane = {
-      imie: document.getElementById('imie').value,
-      nazwisko: document.getElementById('nazwisko').value,
-      pelnoletni: document.getElementById('pelnoletni').checked,
-      kolor: document.getElementById('kolor').value,
-    };
-
-    console.log('Dane z formularza:', dane);
-    alert("Formularz został wysłany (lokalnie)");
-  });
+  if (kolorInput) {
+    kolorInput.addEventListener('input', function () {
+      const wybranyKolor = kolorInput.value;
+      document.body.style.backgroundColor = wybranyKolor;
+    });
+  }
 });
 
 
