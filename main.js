d27564dd25226c89da1ae0a389f09cc4c89113d7
Fademacios <122602505+Fademacios@users.noreply.github.com>
2025-05-09 08:35:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const kolorInput = document.getElementById('kolor');
+
+  kolorInput.addEventListener('input', (event) => {
+    const wybranyKolor = event.target.value;
+    document.body.style.backgroundColor = wybranyKolor;
+  });
+});
 
